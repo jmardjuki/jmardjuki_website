5d9572aac4200a4a7ad4f0759fdd20f93033f0f4
jmardjuki <mardjuki@sfu.ca>
2015-12-21 23:55:21 -0800

    Fixed timing

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#pagepiling').pagepiling({
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-        sectionsColor: ['#2C3E50', '#39C', '#ee005a', '#39C'],
+        sectionsColor: ['#2C3E50', '#39C', '#ee005a', '#39C','#ff0000' ],
         navigation: {
             'position': 'right',
             'tooltips': ['TOP↑', 'About Me', 'Projects', 'Gallery', 'Contact Me']
@@ -14,7 +14,7 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.logo_icon').hover(function() {
             $( this ).fadeOut( 100 );
-            $( this ).fadeIn( 1000 );
+            $( this ).fadeIn( 800 );
             var valueText = this.id;
             valueText = '<span id="logo_hoverText">' + valueText + '</span>'
             $('#logo_hoverText').replaceWith( $(valueText) );
