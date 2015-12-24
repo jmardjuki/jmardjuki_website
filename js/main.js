3431e75f02cac40037333fa0b8f5e51d9af17da2
jmardjuki <mardjuki@sfu.ca>
2015-12-24 23:58:00 +0700

    Added Projects

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
+"use strict"
+
+/* PagePiling */
 $(document).ready(function() {
     $('#pagepiling').pagepiling({
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-        sectionsColor: ['#2C3E50', '#39C', '#ee005a', '#39C','#ff0000' ],
+        sectionsColor: ['#2C3E50', '#39C', '#ee0000', '#39C','#ff0000' ],
         navigation: {
             'position': 'right',
-            'tooltips': ['TOP↑', 'About Me', 'Projects', 'Gallery', 'Contact Me']
+            'tooltips': ['TOP↑', 'About Me', 'Projects', 'Resume', 'Contact Me']
         },
 
     });
 });
 
+/* Others */
 $(document).ready(function() {
+    // TOP Page
     $('.logo_icon').hover(function() {
-            $( this ).fadeOut( 100 );
-            $( this ).fadeIn( 800 );
+            $(this).fadeOut(100);
+            $(this).fadeIn(800);
             var valueText = this.id;
             valueText = '<span id="logo_hoverText">' + valueText + '</span>'
             $('#logo_hoverText').replaceWith( $(valueText) );
@@ -22,5 +27,29 @@ $(document).ready(function() {
             var valueText = '<span id="logo_hoverText">_______</span>'
             $('#logo_hoverText').replaceWith( $(valueText) );        
         }
-    );  
+    );
+    // Projects Page
+    $('.project_select').hover(function() {
+        // Fade
+        //$(this).fadeOut(100);
+        //$(this).fadeIn(200);
+        //$(this).append('*');
+    /*}, function() {
+        var test = $(this).findIndexOf('*');
+        console.log(test);
+    */
+    });
+    $('.project_select').click(function() {
+        var projectIdArray;
+        var projectId = this.id;
+        var yearSelected;
+
+        projectIdArray = projectId.split('_');
+        yearSelected =  projectIdArray[2];
+        yearSelected = "#project_content_" + yearSelected;
+        console.log(yearSelected);
+        $(yearSelected).show();
+
+    });
+       
 });
