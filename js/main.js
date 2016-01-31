f27270612008bb986949f32778f1b69b0ffa4b47
jmardjuki <mardjuki@sfu.ca>
2016-01-30 20:45:49 -0800

    Modified colours and UX

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ $(document).ready(function() {
     $('#pagepiling').pagepiling({
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-        sectionsColor: ['black', '#39C', '#ee0000', '#2C3E50','#ff0000' ],
+        sectionsColor: ['black', '#00004d', '#142639', '#2C3E50','#2C3E00' ],
         navigation: {
+            'textColor': 'white',
+            'bulletsColor': 'white',
             'position': 'right',
             'tooltips': ['TOP↑', 'About Me', 'Projects', 'Resume', 'Contact Me']
         },
@@ -21,6 +23,16 @@ $(document).ready(function() {
 /* Others */
 $(document).ready(function() {
     // TOP Page
+    setInterval(function(){ 
+        var noticeColour = $('#notice_me').css("color");
+        if ( noticeColour == "rgb(255, 0, 0)") {
+            $('#notice_me').css("color", "white");
+        }
+        else{ 
+            $('#notice_me').css("color", "red");
+        }
+    }, 1500);
+
     $('.logo_icon').hover(function() {
             $(this).fadeOut(100);
             $(this).fadeIn(800);
@@ -33,7 +45,21 @@ $(document).ready(function() {
         }
     );
     // Projects Page
-    $('#project_sel_2015').css("color", "black");  // Default
+    $('#project_sel_2015').css("color", "rgb(200, 78, 25)");  // Default
+
+    var oriColour; // Original colour of project select
+    $('.project_select').hover(
+        function() {
+            oriColour = $(this).css("color");
+            if ( oriColour != "rgb(200, 78, 25)" ) {
+                $(this).css("color", "rgb(200, 78, 25)");
+            }            
+        },function() {
+            if ( oriColour != "rgb(200, 78, 25)" ) {
+                $( this ).css("color", "green");
+            }
+        }    
+    );    
 
     $('.project_select').click(function() {
         var projectIdArray;
@@ -41,7 +67,7 @@ $(document).ready(function() {
         var yearSelected;
 
         $('.project_select').css("color", "green");
-        $(this).css("color", "black");
+        $(this).css("color", "rgb(200, 78, 25)");
         projectIdArray = projectId.split('_');
         yearSelected =  projectIdArray[2];
         if ( yearSelected == 2015) {
@@ -59,9 +85,7 @@ $(document).ready(function() {
            $('#project_content_2014').hide();
            $('#project_content_2013').show(); 
         }
-        else {
-            // Do nothing
-        } 
+        // Else do nothing 
 
     });
 
@@ -70,6 +94,9 @@ $(document).ready(function() {
         this.remove();
         var domainNeemu = "sfu.ca";
         var zhiString = '<span>mardjuki' + '@' + domainNeemu + '</span>';
+        var areaCode = '778';
+        var theRest = '846';
+        zhiString = zhiString + ' / ' + areaCode + '.' + theRest;
         $('#contact_me').append(zhiString);
 
     });
